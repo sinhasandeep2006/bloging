6f55654ad30ec4ae5d9d75f0405aafb4cc9e16c7
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-07-17 15:33:21 +0530

    this is the basis of this file

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,14 @@ const router = express.Router()
 
 //Routes
 router.get('/', (req,res)=>{
-    res.render('index');
+    const locals={
+        title:"NodeJs Blog",
+        description:"Simple Blog created with Nodejs, Express & MongoDB."
+    }
+    res.render('index',{locals});
 })
 
+router.get('/about', (req,res)=>{
+    res.render('about');
+})
 module.exports=router
